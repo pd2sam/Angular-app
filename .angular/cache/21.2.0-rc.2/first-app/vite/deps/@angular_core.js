74911d60a409e02e6cfe52f16a0ad59f890f2f9e
pd2sam <pdsam@bk.ru>
2026-03-05 19:43:22 +0300

    routing enabled

--- .angular/cache/21.2.0-rc.2/first-app/vite/deps/@angular_core.js
+++ .angular/cache/21.2.0-rc.2/first-app/vite/deps/@angular_core.js
@@ -502,7 +502,7 @@ import {
   ɵɵvalidateAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-5YZZKYIK.js";
+} from "./chunk-RPLQVTNH.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
